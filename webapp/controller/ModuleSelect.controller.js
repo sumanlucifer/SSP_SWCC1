53552ffb47981ffd5559cbb6c376507f4b10cb2e
srikanthaku <srikanth.akuluru@gmail.com>
2023-11-23 09:06:22 +0000

    test

--- webapp/controller/ModuleSelect.controller.js
+++ webapp/controller/ModuleSelect.controller.js
@@ -8,7 +8,6 @@ sap.ui.define([
 		return BaseController.extend("com.swcc.Template.controller.MouleSelect", {
 			onInit: function () {
 				this.oRouter = this.getRouter();
-				debugger;
 				this.testCPI_API();
 				this.testOdata_API();
 
@@ -24,9 +23,10 @@ sap.ui.define([
 
 			},
 			testOdata_API: function () {
+				debugger;
 				const oModel = this.getOwnerComponent().getModel("ModuleInput");
 
-				this.getAPI.crudOperations_ODATA(oModel, 'read', '/I_Producttype')
+				this.getAPI.crudOperations_ODATA(oModel, 'read', '/ZCDSV_SS_SERVICES_F4')
 					.then(function (oResponse) {
 						var aData = oResponse.data;
 
@@ -36,15 +36,36 @@ sap.ui.define([
 
 			testCPI_API: function () {
 
-				var oOptions = {
-					url: "http/sf",
-					type: "GET"
-				};
-				this.getAPI.crudOperations_REST(oOptions)
-					.then(function (oResponse) {
-						var aData = oResponse.data;
+				var oManifest = this.getOwnerComponent().getManifest();
+				var oDataSources = oManifest["sap.app"].dataSources;
+				var oRemoteService = oDataSources.CPI_API;
 
-					}.bind(this));
+				// Create an OData model using the configured data source
+				var oModel = new sap.ui.model.odata.v2.ODataModel(oRemoteService.uri);
+
+				debugger
+
+				// Perform a read operation to fetch data from the remote API
+				oModel.read("/http/sf", {
+					success: function (data) {
+						// Handle successful data retrieval
+						console.log("Data from remote API:", data);
+					},
+					error: function (error) {
+						// Handle error
+						console.error("Error fetching data from remote API:", error);
+					}
+				});
+
+				// var oOptions = {
+				// 	url: "http/sf",
+				// 	type: "GET"
+				// };
+				// this.getAPI.crudOperations_REST(oOptions)
+				// 	.then(function (oResponse) {
+				// 		var aData = oResponse.data;
+
+				// 	}.bind(this));
 			}
 
 		})
