cf8d877854849afd5868ac1e82b84990884449fc
srikanthaku <srikanth.akuluru@gmail.com>
2023-11-30 11:47:01 +0000

    Commit changes 30-11 02:46 PM

---
+++ webapp/controller/AppHomePage.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+		"./BaseController",
+		"sap/ui/model/json/JSONModel"
+	],
+
+	function (BaseController, JSONModel) {
+		"use strict";
+		return BaseController.extend("com.swcc.Template.controller.AppHomePage", {
+			onInit: function () {
+				this.oRouter = this.getRouter();
+
+			},
+			onPressTile: function (oEvent) {
+				this.getOwnerComponent().getRouter().navTo("ModuleSelect");
+			}
+		})
+	})
