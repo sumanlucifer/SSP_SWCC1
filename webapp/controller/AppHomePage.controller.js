b4b16f3c5cac9de82a62226340fdb7c5f70b7917
srikanthaku <srikanth.akuluru@gmail.com>
2023-12-04 09:57:26 +0000

    test

--- webapp/controller/AppHomePage.controller.js
+++ webapp/controller/AppHomePage.controller.js
@@ -7,6 +7,8 @@ sap.ui.define([
 		"use strict";
 		return BaseController.extend("com.swcc.Template.controller.AppHomePage", {
 			onInit: function () {
+			    
+			    
 
 				this.oRouter = this.getRouter();
 
