95587991cfce9577dbeeb5f866fc75aa7c672521
srikanthaku <srikanth.akuluru@gmail.com>
2023-12-05 16:51:41 +0000

    test

---
+++ webapp/controller/ITCreateRequest.controller.js
@@ -0,0 +1,101 @@
+sap.ui.define([
+		"./BaseController",
+		"sap/ui/model/json/JSONModel",
+		"sap/ui/core/routing/History",
+		"sap/m/MessageBox"
+	],
+
+	function (BaseController, JSONModel, History, MessageBox) {
+		"use strict";
+		return BaseController.extend("com.swcc.Template.controller.ITCreateRequest", {
+			onInit: function () {
+
+				this.oRouter = this.getRouter();
+				this._createItemDataModel();
+
+			},
+			_createItemDataModel: function () {
+				this.getModel().setData({
+					busy: false,
+					recognitionAlreadyStarted: false,
+
+					MarineTransportation: {
+						itemData: []
+					}
+				});
+			},
+			handleBackPress: function () {
+				var oHistory, sPreviousHash;
+				oHistory = History.getInstance();
+				sPreviousHash = oHistory.getPreviousHash();
+				if (sPreviousHash !== undefined) {
+					window.history.go(-1);
+				} else {
+					this.getRouter().navTo("LandingView", {}, true);
+
+				}
+
+			},
+			onback: function () {
+				this.getOwnerComponent().getTargets().display("LandingView");
+
+			},
+			onProceed: function () {
+				//	this.getOwnerComponent().getTargets().display("DetailView");
+				this._handleMessageBoxProceed("Your Service Request has been generated : 12111099");
+			},
+
+			_handleMessageBoxProceed: function (sMessage) {
+				var that = this;
+				sap.m.MessageBox.success(sMessage, {
+					icon: MessageBox.Icon.SUCCESS,
+					title: "Success",
+					actions: [MessageBox.Action.OK],
+					emphasizedAction: MessageBox.Action.YES,
+					onClose: function (oAction) {
+						if (oAction == "OK") {
+
+							that.getRouter().navTo("FinanceRequestView", {}, true);
+						}
+					},
+				});
+
+			},
+			onAddItemsPress: function (oEvent) {
+				var oModel = this.getModel().getProperty("/MarineTransportation/itemData");
+				var oItems = oModel.map(function (oItem) {
+					return Object.assign({}, oItem);
+				});
+				oItems.push({
+					Material: "",
+					Description: "",
+					StorageLocation: "",
+					Quantity: "",
+					BaseUnit: "",
+					Batch: "",
+					M: true,
+					// UnloadPoint: "",
+					AvailableQty: null,
+					PopupItems: null,
+					IsBOQApplicable: ""
+				});
+				this.getModel().setProperty("/MarineTransportation/itemData", oItems);
+
+			},
+			onDeleteItemPress: function (oEvent) {
+				var iRowNumberToDelete = parseInt(oEvent.getSource().getBindingContext().getPath().split("/")[3]);
+				var aTableData = this.getModel().getProperty("/MarineTransportation/itemData");
+				aTableData.splice(iRowNumberToDelete, 1);
+				this.getModel().refresh();
+			},
+			onProceed: function () {
+				this.getOwnerComponent().getTargets().display("HRRequest");
+			},
+
+			onSearch: function () {
+
+				this.oRouter.navTo("LandingView");
+
+			}
+		})
+	})
