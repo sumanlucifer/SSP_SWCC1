692c77b330a945c7c8955ac111694530583a19c2
srikanthaku <srikanth.akuluru@gmail.com>
2023-12-10 12:22:07 +0000

    commit changes

---
+++ webapp/controller/PMViewRequest.controller.js
@@ -0,0 +1,76 @@
+sap.ui.define([
+		"./BaseController",
+		"sap/ui/model/json/JSONModel",
+		"sap/ui/core/routing/History",
+		"sap/m/MessageBox"
+	],
+
+	function (BaseController, JSONModel, History, MessageBox) {
+		"use strict";
+		return BaseController.extend("com.swcc.Template.controller.FinanceViewRequest", {
+			onInit: function () {
+
+				this.oRouter = this.getRouter();
+
+			},
+
+			_createItemDataModel: function () {
+				this.getModel().setData({
+					busy: false,
+					recognitionAlreadyStarted: false,
+
+					MarineTransportation: {
+						itemData: []
+					}
+				});
+			},
+			handleBackPress: function () {
+				var oHistory, sPreviousHash;
+				oHistory = History.getInstance();
+				sPreviousHash = oHistory.getPreviousHash();
+				if (sPreviousHash !== undefined) {
+					window.history.go(-1);
+				} else {
+					this.getRouter().navTo("LandingView", {}, true);
+				}
+
+			},
+			onback: function () {
+				this.getOwnerComponent().getTargets().display("LandingView");
+
+			},
+			onAddItemsPress: function (oEvent) {
+				var oModel = this.getModel().getProperty("/MarineTransportation/itemData");
+				var oItems = oModel.map(function (oItem) {
+					return Object.assign({}, oItem);
+				});
+				oItems.push({
+					Material: "",
+					Description: "",
+					StorageLocation: "",
+					Quantity: "",
+					BaseUnit: "",
+					Batch: "",
+					M: true,
+					// UnloadPoint: "",
+					AvailableQty: null,
+					PopupItems: null,
+					IsBOQApplicable: ""
+				});
+				this.getModel().setProperty("/MarineTransportation/itemData", oItems);
+
+			},
+			onDeleteItemPress: function (oEvent) {
+				var iRowNumberToDelete = parseInt(oEvent.getSource().getBindingContext().getPath().split("/")[3]);
+				var aTableData = this.getModel().getProperty("/MarineTransportation/itemData");
+				aTableData.splice(iRowNumberToDelete, 1);
+				this.getModel().refresh();
+			},
+
+			onSearch: function () {
+
+				this.oRouter.navTo("LandingView");
+
+			}
+		})
+	})
