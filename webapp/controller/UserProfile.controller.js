f87b34fa112abb1a9dc0af4335ca8e35e5605855
srikanthaku <srikanth.akuluru@gmail.com>
2023-12-25 19:13:20 +0000

    commit

--- webapp/controller/UserProfile.controller.js
+++ webapp/controller/UserProfile.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
-		"./BaseController",
-		"sap/ui/model/json/JSONModel",
-		"sap/ui/core/routing/History"
-	],
+	"./BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History"
+],
 
-	function (BaseController, JSONModel, History) {
-		"use strict";
-		return BaseController.extend("com.swcc.Template.controller.UserProfile", {
+function (BaseController, JSONModel, History) {
+	"use strict";
+	return BaseController.extend("com.swcc.Template.controller.UserProfile", {
 
 			onInit: function () {
 
@@ -14,15 +14,10 @@ sap.ui.define([
 
 			},
 			handleBackPress: function () {
-				var oHistory, sPreviousHash;
-				oHistory = History.getInstance();
-				sPreviousHash = oHistory.getPreviousHash();
-				if (sPreviousHash !== undefined) {
-					window.history.go(-1);
-				} else {
-					this.getRouter().navTo("HomePage", {}, true);
-				}
+				this.navigationBack();
 
-			}
-		})
+			},
+			
+		}
 	})
+})
