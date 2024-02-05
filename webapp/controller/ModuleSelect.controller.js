a599d57702ba172337aa1a846fa01f828f5332ef
srikanthaku <srikanth.akuluru@gmail.com>
2024-02-05 09:07:56 +0000

    commit changes

--- webapp/controller/ModuleSelect.controller.js
+++ webapp/controller/ModuleSelect.controller.js
@@ -108,10 +108,9 @@ sap.ui.define([
 				}
 
 			},
-			/*onback: function () {
-				this.getOwnerComponent().getTargets().display("LandingView");
-
-			},*/
+			handleBackHomePress: function () {
+				this.getRouter().navTo("HomePage", {}, true);
+			},
 
 			onSearch: function () {
 
